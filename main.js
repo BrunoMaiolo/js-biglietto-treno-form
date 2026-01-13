de11e50146a22ecb14e048aba3a3b043f235961f
BrunoMaiolo <brunosj@hotmail.it>
2026-01-13 14:46:37 +0100

    sconti -20% e -40%

--- main.js
+++ main.js
@@ -10,5 +10,13 @@ const km = Number(kmInput.value);
   const age = Number(ageInput.value);
   // calcolo del prezzo base
   let price = km * 0.21;
+  //sconti (-20% , -40%)
+  if (age < 18) {
+    price = price * 0.8; 
+  } else if (age >= 65) {
+    price = price * 0.6; 
+  }
+  //Stampa in console
+  
 
 });
